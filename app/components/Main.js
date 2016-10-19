f826bfac35dec10505d9444289e05fde889bd637
Meredith Nachman <mwnachman@gmail.com>
2016-10-19 16:58:13 -0700

    webpack setup is working to bundle files

---
+++ app/components/Main.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var Main = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Testing
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(<Main />, document.getElementById('app'));
