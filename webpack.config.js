0b0661c6a5a40af2a4beeee00229aee4f131265d
Meredith Nachman <mwnachman@gmail.com>
2016-10-19 16:58:13 -0700

    webpack setup is working to bundle files

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+  entry: "./app/components/Main.js", 
+  output: {
+    filename: "public/bundle.js"
+  }, 
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        resolveLoader: {
+          root: path.join(__dirname, 'node_modules')
+        },
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
