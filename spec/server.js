d20ea3274491a6b9538bef45525d00306132628b
William Simmons <willsimmons@gmail.com>
2016-10-19 16:23:53 -0700

    blank test file added

--- spec/server.js
+++ spec/server.js
@@ -1,14 +1,2 @@
 const express = require('express');
 const expect = require('chai').expect;
-const app = require('../config.js');
-
-describe('The Server runs', () => {
-
-  it('replys when get is sent to root route', (done) => {
-    request(app)
-      .get('/')
-      .expect(200)
-      .end(done);
-  });
-
-});
