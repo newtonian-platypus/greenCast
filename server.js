610a76af5c5e75529b94638951021b04cbb4673d
William Simmons <willsimmons@gmail.com>
2016-10-19 16:21:14 -0700

    abstracted some of server's work to routes file, start test work

---
+++ server.js
@@ -0,0 +1,18 @@
+//should be abstracted into a server config file
+const express = require('express');
+const PORT = require('./server/config/config.js').PORT;
+const betterErrors = require('better-express-errors');
+const bodyParser = require('body-parser');
+const Promise = require('bluebird');
+const app = express();
+const routes = require('./server/routes.js');
+
+//basic router - could be refactored into a 
+app.get('/', (req, res) => routes.root(req, res));
+
+app.listen(PORT, () => console.log(`GreenCast listening on port ${PORT}`));
+
+//run this after all routes and middlware
+app.use(betterErrors(app));
+
+module.exports = app;
