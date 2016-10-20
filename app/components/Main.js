9bbfb131d17d32d09cd839ec2e66ad73786ff114
William Simmons <willsimmons@gmail.com>
2016-10-20 11:37:21 -0700

    build nav view

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,20 +1,14 @@
 var React = require('react');
+var NavView = require('./NavView');
 
 
 var Main = React.createClass({
   render: function() {
     return (
       <div className="main-container">
-        <nav className="navbar navbar-default" role="navigation">
-          <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
-            MENU
-          </div>
-        </nav>
-        <div className="container">
-          {this.props.children}
-        </div>
+         <NavView />
       </div>
-    )
+    );
   }
 });
 
---
+++ app/components/NavView.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var NavView = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <span>test</span>
+      </div>
+    );
+  }
+});
+
+module.exports = NavView;
