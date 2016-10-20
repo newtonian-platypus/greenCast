fd729c12170d1a0f4f24bbc14a73048d6578d777
William Simmons <willsimmons@gmail.com>
2016-10-19 20:22:14 -0700

    port fix

--- server/config.example.js
+++ server/config.example.js
@@ -1,4 +1,4 @@
 // our environmental variables
-exports.PORT = 'PORT # HERE';
+exports.port = 'PORT # HERE';
 exports.AUDIOSEARCH_APP_ID = 'YOUR APP ID HERE';
 exports.AUDIOSEARCH_SECRET = 'YOUR SECRET HERE';
