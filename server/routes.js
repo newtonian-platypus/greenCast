a1505c2b0a85b4d838a21709167de2df8d042779
Chase Starr <chasestarr@gmail.com>
2016-10-20 19:30:34 -0700

    add initial server routes with function descriptions

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,33 @@
+const root = (req, res) => {
+  res.status(200);
+  res.send('hello world');
+};
+
+
+// routes for user data
+const addUser = (req, res) => {
+  // creates a new user with email address
+};
+
+const getUser = (req, res) => {
+  // returns a user's data
+};
+
+const getSubscriptions = (req, res) => {
+  // returns a user's subscriptions
+};
+
+const addSubscription = (req, res) => {
+  // adds a channel to a user's subscriptions
+}
+
+// routes for channel data
+const getEpisodes = (req, res) => {
+  // grabs rss data, scrapes it, and returns array of episodes
+};
+
 module.exports = {
-  
-  root: function(req, res) {
-    res.send('hello world');
-  }
+  root: root,
+  getEpisodes: getEpisodes,
+  createUser: createUser
 };
