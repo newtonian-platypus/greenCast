8328c8a8bb40b9d7beaa83f005ad8727ff9d10a0
Chase Starr <chasestarr@gmail.com>
2016-10-20 19:43:36 -0700

    add route handlers to server.js

--- server.js
+++ server.js
@@ -6,8 +6,23 @@ const Promise = require('bluebird');
 const app = express();
 const routes = require('./server/routes.js');
 
-//basic router - could be refactored into a 
-app.get('/', (req, res) => routes.root(req, res));
+//basic router - could be refactored into a
+app.get('/', routes.root);
+
+// returns a user's subscriptions
+app.get('/user/:username/subscriptions', routes.getSubscriptions);
+
+// adds a new channel to a user's subscriptions
+app.post('/user/:username/subscriptions', routes.addSubscription);
+
+// get's a user's public information
+app.get('/user/:username', routes.getUser);
+
+// adds a new user
+app.post('/user/:username', routes.getUser);
+
+// returns an array of a channel's episodes
+app.get('/channel', routes.getEpisodes);
 
 app.listen(3000, () => console.log('GreenCast listening on port 3000'));
 
