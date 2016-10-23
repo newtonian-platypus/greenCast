be83499ed3339338847ca8574d3edf95a228bb79
Chase Starr <chasestarr@gmail.com>
2016-10-22 18:23:15 -0700

    UserModel.findOne test passing

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    unique: true
+  },
   subscriptions: Array
 });
 
