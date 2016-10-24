03bcdbe1919ecbefcf8487755b0055ce934de529
Chase Starr <chasestarr@gmail.com>
2016-10-24 11:49:49 -0700

    (test) add POST /user test (feature) create podcast lookup file at /server/requestPodcastData.js

---
+++ server/requestPodcastData.js
@@ -0,0 +1,40 @@
+const request = require('request');
+const parsePodcast = require('node-podcast-parser');
+const lookupEndpoint = 'http://itunes.apple.com/lookup?id=';
+
+const itunesLookup = (channelId, cb) => {
+  request(lookupEndpoint + channelId, (err, res, body) => {
+    if (err) cb(err, null);
+    cb(err, body);
+  });
+};
+
+const requestRss = (rssUrl, cb) => {
+  request(rssUrl, (err, res, body) => {
+    if (err) cb(err, null);
+    parsePodcast(body, (err, data) => {
+      if (err) cb(err, null);
+      cb(err, data);
+    });
+  });
+};
+
+module.exports = {
+  itunesLookup: itunesLookup,
+  requestRss: requestRss
+};
+
+
+// ** Example Usage **
+// itunesLookup(917918570, (err, podcasts) => {
+//   if (err) console.log(err);
+
+//   const podcast = JSON.parse(podcasts).results[0];
+//   requestRss(podcast.feedUrl, (err, feed) => {
+//     if (err) console.log(err);
+//     console.log('title', feed.title);
+//     console.log('link', feed.link);
+//     console.log('short', feed.description.short);
+//     console.log('latest episode', feed.episodes[0]);
+//   });
+// });
