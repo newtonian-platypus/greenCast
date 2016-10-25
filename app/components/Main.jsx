240b206a1b93389b25976650cec2a6ea73b54a69
Chase Starr <chasestarr@gmail.com>
2016-10-25 11:59:47 -0700

    add react-audio-player, add nowPlaying state parameter

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -11,7 +11,8 @@ class Main extends React.Component {
     super();
 
     this.state = {
-      subscriptions: [496893300, 447667314, 1066446588]
+      subscriptions: [496893300, 447667314, 1066446588],
+      nowPlaying: 'https://dts.podtrac.com/redirect.mp3/dovetail.prxu.org/serial/d7f03a15-be26-4634-8884-5fadd404ad75/serial-s01-e01.mp3'
     };
   }
 
@@ -33,7 +34,7 @@ class Main extends React.Component {
            showEpisodes={this.showEpisodes.bind(this)}
          />
          <FeedView />
-         <PlayerView />
+         <PlayerView nowPlaying={this.state.nowPlaying}/>
       </div>
     );
   }
