365c5cdfeb7b9d2952a7934ccc0b1926fa7f31e5
William Simmons <willsimmons@gmail.com>
2016-10-25 17:09:28 -0700

    added key/index to subscribed channel view (to remove a react console warning).
    first draft of feed view - new issues
    -thumbnails?
    -styling
    -duration to actual minutes
    -clickable activity

--- app/components/FeedView.jsx
+++ app/components/FeedView.jsx
@@ -1,22 +1,61 @@
 import React from 'react';
+import Promise from 'bluebird';
+import $ from 'jquery';
 import FeedItemView from './FeedItemView.jsx';
+
 
 class FeedView extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      episodeList: []
+    };
+  }
+
+  componentWillMount() {
+    const context = this;
+    //this should hit server API or itunes API, need to talk about this!
+    // Would have to promisify this when async, using dummy data now.
+    // Promise.all(data).done(results => {
+    //   context.setState({
+        // episodes: results.map(episode => episodes.push(episode))
+      // });
+    // });
+    //in the meantime, using dummy data
+    this.props.currentFeed.map(episode => this.state.episodeList.push({
+      'title': episode.title,
+      'description': episode.description,
+      'duration': episode.duration
+    }));
   }
 
   render() {
     return (
-      <div>
-        <span>FeedView</span>
-         <div>
-           <FeedItemView />
-         </div>
+      <div style={styles.feedView}> {this.props.currentFeedTitle}
+        {
+          this.state.episodeList.map((episode, index) =>
+            <FeedItemView key={index} episode = {episode}/>
+          )
+        }
       </div>
     );
   }
+
+  requestPodcastData(id) {
+    return $.ajax({
+      url: `http://itunes.apple.com/lookup?id=${id}`,
+      method: 'GET',
+      dataType: 'JSONP'
+    });
+  }
 }
+
+const styles = {
+  feedView: {
+    background: 'orange'
+  }
+};
 
 export default FeedView;
