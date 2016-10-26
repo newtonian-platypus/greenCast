2ebe170cf76e7d6d43acfc6f7e850455d2bbb5ab
William Simmons <willsimmons@gmail.com>
2016-10-26 13:01:30 -0700

    create function to get a channel feed

--- server/requestPodcastData.js
+++ server/requestPodcastData.js
@@ -4,24 +4,47 @@ const lookupEndpoint = 'http://itunes.apple.com/lookup?id=';
 
 const itunesLookup = (channelId, cb) => {
   request(lookupEndpoint + channelId, (err, res, body) => {
-    if (err) cb(err, null);
+    if (err) {
+      cb(err, null);
+    } 
     cb(err, body);
   });
 };
 
 const requestRss = (rssUrl, cb) => {
   request(rssUrl, (err, res, body) => {
-    if (err) cb(err, null);
+    if (err) { 
+      cb(err, null);
+    }
     parsePodcast(body, (err, data) => {
-      if (err) cb(err, null);
+      if (err) {
+        cb(err, null);
+      }
       cb(err, data);
+    });
+  });
+};
+
+const feedGenerator = (channelId) => {
+  itunesLookup(channelId, (err, podcasts) => {
+    if (err) { 
+      console.log(err);
+    }
+    const podcast = JSON.parse(podcasts).results[0];
+    requestRss(podcast.feedUrl, (err, feed) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log('feedGenerator feed', feed);
+      return feed;
     });
   });
 };
 
 module.exports = {
   itunesLookup: itunesLookup,
-  requestRss: requestRss
+  requestRss: requestRss,
+  feedGenerator: feedGenerator
 };
 
 
