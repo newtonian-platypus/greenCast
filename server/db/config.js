385700aca8dfce0eaac11f8a0cbe4f7d60c16147
William Simmons <willsimmons@gmail.com>
2017-03-15 13:42:12 -0700

    connecting

--- server/db/config.js
+++ server/db/config.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 // mongoURI = 'mongodb://localhost/greenCast';
 // mongoURI = 'mongodb://<dbuser>:<dbpassword>@ds017736.mlab.com:17736/heroku_b2pmb0gd';
-mongoose.connect(mongoURI);
+// mongoose.connect(mongoURI);
+mongoose.connect(MONGOLAB_MAUVE_URI);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
