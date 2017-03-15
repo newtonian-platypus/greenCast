c559da108ad08b168cafd7d8a6a4344f304926b8
William Simmons <willsimmons@gmail.com>
2017-03-15 16:25:08 -0700

    fix

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 3000;
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL:'https://obscure-forest-62580.herokuapp.com'
+  callbackURL:'https://obscure-forest-62580.herokuapp.com/auth/github/callback'
   // callbackURL: 'https://www.greencast.me.herokudns.com/auth/github/callback'
   // callbackURL: 'http://localhost:3000/auth/github/callback'
 }, routes.login));
